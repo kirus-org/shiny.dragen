914d69b077fa61df1880b6a12f3e1bac6bbb8bdd
kmezhoud <kmezhoud@gmail.com>
2024-05-16 13:35:20 +0100

    minor

--- Inst/shiny.dragen/server.R
+++ Inst/shiny.dragen/server.R
@@ -1,11 +1,15 @@
 shinyServer(function(input, output, session) {
   
+  # increase limite size file to  120MB
+  options(shiny.maxRequestSize=120*1024^2) 
+  
+  source("vcf_server.R", encoding = "UTF-8", local = TRUE)
+  source("vcf_ui.R", encoding = "UTF-8", local = TRUE)
   
   ## stop shiny app when browser is closed
   session$onSessionEnded(function() {
     stopApp()
     print("The shiny.dragen session is closed.")
   })
-  
   
 })
--- Inst/shiny.dragen/ui.R
+++ Inst/shiny.dragen/ui.R
@@ -2,28 +2,32 @@ suppressMessages({
   library(shiny)
   library(shinythemes)
   library(shinydashboard)
+  #library(shinyWidgets)
   library(knitr)
 })
 
 shinyUI(fluidPage(theme = shinytheme("flatly"), title = "shiny.dragen", #superhero, flatly, united
                   ## add the icon logo to browser tab
                   tags$head(
-                    HTML("<title>shiny.dragen</title> <link rel='icon' type='image/gif/png' href='logo.png'>")
+                    HTML("<title>shiny.dragen</title> <link rel='icon' type='image/gif/png' href='dragen_f29405.png'>")
                   ),
                   
                   tags$head(tags$script(type="text/javascript", src = "octopus.js")),
                   
                   ## Include logo in navbar
-                  navbarPage(title=div(img(src="logo.png", height = "50px", collapsable=TRUE, #width = "50px",
+                  navbarPage(title=div(img(src="dragen_f29405.png", height = "50px", collapsable=TRUE, #width = "50px",
                                            style = "position: relative; top: -14px; right: 1px;"),
-                                       "shiny.dragen"),
+                                       tags$b("dragen"), style="color:#FFB266"),
                              
+                             #tabPanel("dragen", icon = icon('dragon')),
                              
-                             tabPanel("Globe",icon = icon('globe'),
+                            # tabPanel("fastq", icon= icon('dna')),
+                             
+                             tabPanel("VCF",icon = icon('table'),
                                       # div(class="outer",
                                       #     tags$head(includeCSS("www/styles.css")),
                                       #     
-                                      #     uiOutput('ui_frontPage')
+                                           uiOutput('ui_vcf')
                                       # )
                                       ),
                              
